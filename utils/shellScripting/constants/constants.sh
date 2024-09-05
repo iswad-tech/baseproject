3a24b70dd686930c08eb00f21129423f615cee2d
Mohammad Mahdi Mohajer <mmmohajer70@gmail.com>
2024-09-05 19:27:32 -0400

    Project is initiated

--- utils/shellScripting/constants/constants.sh
+++ utils/shellScripting/constants/constants.sh
@@ -2,13 +2,13 @@ BOLD_TXT=$(tput bold)
 NORMAL_TXT=$(tput sgr0)
 OPTIONS=(0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 Q)
 
-STAGING_SERVER_ALIAS=IswadStaging
-PROD_SERVER_ALIAS=IswadProd
+STAGING_SERVER_ALIAS=BaseProject
+PROD_SERVER_ALIAS=BaseProject
 
-NGINX_REPO_STAGING="iswad/website-staging-nginx"
-CLIENT_REPO_STAGING="iswad/website-staging-client"
-API_REPO_STAGING="iswad/website-staging-api"
+NGINX_REPO_STAGING="iswad/baseproject-nginx"
+CLIENT_REPO_STAGING="iswad/baseproject-client"
+API_REPO_STAGING="iswad/baseproject-api"
 
-NGINX_REPO="iswad/website-prod-nginx"
-CLIENT_REPO="iswad/website-prod-client"
-API_REPO="iswad/website-prod-api"
+NGINX_REPO="iswad/baseproject-nginx"
+CLIENT_REPO="iswad/baseproject-client"
+API_REPO="iswad/baseproject-api"
